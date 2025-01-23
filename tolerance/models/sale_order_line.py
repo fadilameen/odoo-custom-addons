987ed849b57c977e6a1b2e3d8db5de07c1cb6a59
fadilameen <fadilameen63@gmail.com>
2025-01-23 11:00:01 +0530

    sale order line to stock move by overriding create function simplest

--- tolerance/models/sale_order_line.py
+++ tolerance/models/sale_order_line.py
@@ -12,9 +12,3 @@ class SaleOrderLine(models.Model):
     def _compute_tolerance(self):
         for line in self:
             line.tolerance = line.order_id.partner_id.tolerance
-
-    # def _prepare_procurement_values(self, **kwargs):
-    #     values = super()._prepare_procurement_values(
-    #         **kwargs)
-    #     values['tolerance'] = self.tolerance
-    #     return values
--- tolerance/models/stock_move.py
+++ tolerance/models/stock_move.py
@@ -7,11 +7,12 @@ class StockMove(models.Model):
 
     tolerance = fields.Float(string="Tolerance")
 
-    # @api.model_create_multi
-    # def create(self, vals):
-    #     for val in vals:
-    #         if not val.get('tolerance') and 'sale_line_id' in val:
-    #             sale_line = self.env['sale.order.line'].browse(
-    #                 val['sale_line_id'])
-    #             val['tolerance'] = sale_line.tolerance
-    #     return super(StockMove, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals):
+        for val in vals:
+            sale_line_id = val.get('sale_line_id')
+            tolerance = self.env["sale.order.line"].browse(
+                sale_line_id).tolerance
+            print(tolerance)
+            val['tolerance'] = tolerance
+        return super(StockMove, self).create(vals)
