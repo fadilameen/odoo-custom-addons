f53b9f750dfb9451a69cf2ec3a17633b2e466d35
fadilameen <fadilameen63@gmail.com>
2025-02-11 17:31:44 +0530

    bom in cart start

---
+++ bom_in_cart/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bill of Materials in Cart',
+    'version': '18.0.1.0.0',
+    'summary': 'Display bill of materials in cart in website',
+    'sequence': 1,
+    'description': """"
+Bill of Materials in Cart
+====================
+To display bill of materials in website cart.
+""",
+    # 'category': 'Sales',
+    # 'website': '',
+    # 'depends': ['base', 'sale_management', 'stock', 'purchase', ],
+    # 'data': ['views/res_partner_views.xml',
+    #          'security/ir.model.access.csv',
+    #          'views/sale_order_line_views.xml',
+    #          'views/purchase_order_line_views.xml',
+    #          'views/stock_move_views.xml',
+    #          'views/tolerance_warning_views.xml',
+    #          ],
+    'installable': True,
+    'application': "False",
+
+    'author': 'Fadil',
+    'license': 'LGPL-3',
+
+}
