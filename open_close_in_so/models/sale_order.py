091d67cde8d65270803eb6a10b380db95f1b4818
fadilameen <fadilameen63@gmail.com>
2025-02-13 18:40:12 +0530

    feb 13 6.40PM

---
+++ open_close_in_so/models/sale_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+
+class SaleOrder(models.Model):
+    """model for adding new two state into sale order"""
+    _inherit = 'sale.order'
+
+    current_state = fields.Selection(default='open',
+                                     selection=[('open', 'Open'),
+                                                ('close', 'Close')],
+                                     compute='_compute_current_state',
+                                     required=True, store=True)
+
+    @api.depends("delivery_status")
+    def _compute_current_state(self):
+        """to compute current state based delivery status"""
+        for record in self:
+            if record.delivery_status == 'full':
+                record.current_state = 'close'
+            else:
+                record.current_state = record.current_state
