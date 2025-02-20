d2b0c8b8116ad70a6e1c183a18dd437d622279ba
fadilameen <fadilameen63@gmail.com>
2025-02-20 23:01:29 +0530

    chnged function to set discount

--- pos_category_wise_discount/static/src/js/pos_order_line.js
+++ pos_category_wise_discount/static/src/js/pos_order_line.js
@@ -2,13 +2,10 @@
 
 import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
 import { patch } from "@web/core/utils/patch";
-//import { useService } from "@web/core/utils/hooks";
 
 patch(PosOrderline.prototype, {
-  getDisplayData() {
-    //                             this.notification = useService("notification");
-
-    var res = super.getDisplayData();
+  set_discount(discount) {
+    var res = super.set_discount(discount);
     var discount_limited_category_id =
       this.models["pos.config"].getFirst().pos_category_id.name;
     var product_category_id = this.product_id.pos_categ_ids[0].id;
@@ -24,10 +21,9 @@ patch(PosOrderline.prototype, {
       var discount_limit = this.models["pos.config"].getFirst().discount_limit;
       var current_discount = this.discount;
       if (current_discount > discount_limit) {
-        //                     this.notification = useService("notification");
-        //              this.notification.add(_t("discount paali."));
-        //            return this.props.close();
+
         window.alert("Enter a valid discount!");
+console.log("testtt")
       }
     }
     return res;
