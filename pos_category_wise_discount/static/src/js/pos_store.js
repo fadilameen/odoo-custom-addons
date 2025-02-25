862ff587e71ec4fecaebd80cc07d4cdac86bf1e0
fadilameen <fadilameen63@gmail.com>
2025-02-25 15:39:37 +0530

    task category wise completed. updation left total discount of that category

--- pos_category_wise_discount/static/src/js/pos_store.js
+++ pos_category_wise_discount/static/src/js/pos_store.js
@@ -12,13 +12,9 @@ patch(PosStore.prototype, {
       var discount_limited_category =
         this.models["pos.config"].getFirst().pos_category_id.name;
       var product_categories = [];
-      for (
-        let index = 0;
-        index < line.product_id.pos_categ_ids.length;
-        index++
-      ) {
-        product_categories.push(line.product_id.pos_categ_ids[index].name);
-      }
+      line.product_id.pos_categ_ids.forEach((category) => {
+        product_categories.push(category.name);
+      });
       if (product_categories.includes(discount_limited_category)) {
         var discount_limit =
           this.models["pos.config"].getFirst().discount_limit;
@@ -33,8 +29,32 @@ patch(PosStore.prototype, {
     }
     return res;
   },
-  pay(){
-  console.log(this)
-        var res = super.pay();
-  }
+  pay() {
+    if (this.models["pos.config"].getFirst().is_category_wise_discount_in_pos) {
+      var discount_limited_category =
+        this.models["pos.config"].getFirst().pos_category_id.name;
+      var order_lines = this.get_order().get_orderlines();
+      var discount_exceeded = order_lines.some((line) => {
+        var categories = [];
+        line.product_id.pos_categ_ids.forEach((category) => {
+          categories.push(category.name);
+        });
+        if (categories.includes(discount_limited_category)) {
+          var discount_limit =
+            this.models["pos.config"].getFirst().discount_limit;
+          var current_discount = line.discount;
+          if (current_discount > discount_limit) {
+            this.dialog.add(AlertDialog, {
+              title: _t("Alert!"),
+              body: _t("Some of the product discount exceeded the limit."),
+            });
+            return true;
+          }
+        }
+      });
+      if (!discount_exceeded) super.pay();
+    } else {
+      super.pay();
+    }
+  },
 });
