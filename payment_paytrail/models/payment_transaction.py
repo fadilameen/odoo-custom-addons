8693b2fce5cf1ff981da4e876bde58b9af532ddd
fadilameen <fadilameen63@gmail.com>
2025-03-08 01:22:36 +0530

    Hell YEAHHH... payment created succesfully

--- payment_paytrail/models/payment_transaction.py
+++ payment_paytrail/models/payment_transaction.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import hashlib
-import hmac
 import json
 import uuid
 
+from dateutil.utils import today
+from werkzeug import urls
 from odoo import models, fields
 
 
@@ -11,68 +11,27 @@ class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
     def _get_specific_processing_values(self, processing_values):
-        # print(processing_values, "processing values")
         res = super()._get_specific_processing_values(processing_values)
         if self.provider_code != 'paytrail':
             return res
-        self._get_paytrail_request(processing_values)
-        return res
+        payload = self._paytrail_prepare_payment_request_payload()
+        self.provider_id._paytrail_make_request(payload)
 
-    # def _get_paytrail_items(self, processing_value):
-    #     items = []
-    #     for sale_order in self.sale_order_ids:
-    #         for order_line in (sale_order.order_line):
-    #             print(order_line.read())
-    #             # items += {
-    #             #     "unitPrice": order_line.price_unit,
-    #             #     "units": order_line.product_qty,
-    #             #     "vatPercentage": 25.5,
-    #             #     "productCode": "#1234",
-    #             #     "deliveryDate": "2018-09-01",
-    #             # }
-    def calculate_hmac(self, secret: str, headerParams, body):
-        data = []
-        for key, value in headerParams.items():
-            if key.startswith('checkout-'):
-                data.append('{key}:{value}'.format(key=key, value=value))
-        data.append(body)
-        print(data)
-        print(self.compute_sha256_hash('\n'.join(data), secret))
-
-    def compute_sha256_hash(self, message, secret):
-        # whitespaces that were created during json parsing process must be removed
-        hash = hmac.new(secret.encode(), message.encode(), digestmod=hashlib.sha256)
-        return hash.hexdigest()
-
-    def _get_paytrail_request(self, processing_values):
-        # print(self.read(), "read")
-        # items = self._get_paytrail_items(processing_values)
-        headers = self.provider_id._prepare_paytrail_header()
-        body = {
+    def _paytrail_prepare_payment_request_payload(self):
+        base_url = self.get_base_url()
+        redirect_url = urls.url_join(base_url, '/contactus-thank-you')
+        # print(self.read())
+        body = dict({
             "stamp": str(uuid.uuid4()),
             "reference": self.reference,
             "amount": self.amount,
-            "currency": self.currency_id.name,
-            "language": "EN",
-            "items": [
-                {
-                    "unitPrice": self.amount,
-                    "units": 1,
-                    "vatPercentage": 0,
-                    "productCode": "#1",
-                    "deliveryDate": str(fields.Datetime.today()),
-                },
-            ],
-            "customer": {
-                "email": self.partner_email,
-            },
-            "redirectUrls": {
-                "success": "http://localhost:8018/shop/payment/success",
-                "cancel": "http://localhost:8018/shop/payment/cancel",
-            },
-        }
-        body_json = json.dumps(body, separators=(',', ':'))
-        encData = self.calculate_hmac(self.provider_id.paytrail_secret_key, headers, body_json)
-        # print("Encrypted data: " + encData)
-        # print(body)
+            "currency": "EUR",
+            "language": 'EN',
+            "items": [{"unitPrice": self.amount, "units": 1, "vatPercentage": 0, "productCode": "#1234",
+                       "deliveryDate": str(fields.Date.today())}],
+            "customer": {"email": self.partner_email},
+            "redirectUrls": {"success": redirect_url, "cancel": redirect_url}
+        })
+        payload = json.dumps(body, separators=(',', ':'))
         # print(body_json)
+        return payload
